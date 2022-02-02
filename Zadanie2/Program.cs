456facf6b0ec7674b34438a947d6c9704b64920f
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-02 13:55:40 +0300

    addedCode

---
+++ Zadanie2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int a, b;
+Console.WriteLine("Введите два числа ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else if (a > b)
+{
+    Console.WriteLine("Наибольшее число " + a);
+}
+else
+    Console.WriteLine("Наименьшее число " + a);
