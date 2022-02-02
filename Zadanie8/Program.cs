3a40d019e18fd73e2292d187cb24f00fa11c0ec6
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-02 15:02:32 +0300

    Newcode

---
+++ Zadanie8/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать чётные числа от 1 до N.
+int a;
+int i = 1;
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+while  (i < a)
+{
+    Console.WriteLine(i+1);
+    i+= 2;
+}
