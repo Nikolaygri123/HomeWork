64eaa89362a50dd975a6d9a5afbb6dba6091e1cd
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-02 15:31:07 +0300

    testcode

---
+++ test/Program.cs
@@ -0,0 +1,6 @@
+﻿ 
+Console.WriteLine("Введите трехзначное число ");
+string a =Console.ReadLine();
+Console.WriteLine("Последний символ " + a[2]);
+Console.WriteLine("Средний символ " + a[1]);
+Console.WriteLine("Первый символ " + a[0]);
