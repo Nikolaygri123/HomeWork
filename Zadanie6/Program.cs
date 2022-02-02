010764c3414fd916c745f70adf396017e53afdc6
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-02 14:18:20 +0300

    addednewcode

---
+++ Zadanie6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Выяснить является ли число чётным
+int a;
+Console.WriteLine("Введите число ");
+a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine(a + " - Четное число");
+}
+else 
+Console.WriteLine(a + " - нечетное число");
