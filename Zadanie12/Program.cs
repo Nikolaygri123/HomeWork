712f458f9f0538131e24dca9499b3ba326bd4e55
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-06 15:43:18 +0300

    Zadanie12

---
+++ Zadanie12/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа.
+int a;
+Console.WriteLine("Введите трехзначное число ");
+a = Convert.ToInt32(Console.ReadLine());
+int sim = a % 10;
+int sim2 = a / 100;
+//Console.WriteLine("Вторая цифра трехзначного числа " + sim2);
+Console.WriteLine("Число с удаленной второй цифрой " + sim2 + sim);
