f5fa1064a05782b830ad079bffd40f344a1f86a8
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-06 18:39:16 +0300

    Zadanie16

---
+++ Zadanie16/Program.cs
@@ -0,0 +1,22 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int D;
+Console.WriteLine("Введите номер дня недели от 1 до 7 ");
+D = Convert.ToInt32(Console.ReadLine());
+if (D > 5 && D < 8) 
+{
+    Console.Write("Выходной день - ");
+    if (D == 6)
+    {
+        Console.WriteLine("Суббота ");
+    }
+    if (D == 7)
+    {
+        Console.WriteLine("Воскресенье ");
+    }
+}
+else if (D > 0 && D < 5)
+{
+    Console.WriteLine("Серые будни ");
+}
+else 
+Console.WriteLine("Нет такого дня ");
