ac711c4a8dcc199a0721f0338451c7279b4e5499
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-06 15:25:15 +0300

    Zadanie10

---
+++ Zadanie10/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Показать вторую цифру трёхзначного числа.
+int a;
+Console.WriteLine("Введите трехзначное число ");
+a = Convert.ToInt32(Console.ReadLine());
+int sim = a / 10;
+int sim2 = sim % 10;
+Console.WriteLine("Вторая цифра трехзначного числа " + sim2);
