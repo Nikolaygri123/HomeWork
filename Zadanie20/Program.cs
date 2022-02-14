3397927c7f9f3a47ccb6c555abfe0eb01758f61d
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-14 14:22:51 +0300

    code

---
+++ Zadanie20/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите x: ");
+ double x = Convert.ToDouble(Console.ReadLine());
+
+ Console.WriteLine("Введите y: ");
+ double y = Convert.ToDouble(Console.ReadLine());
+
+ if ((x > 0) && (y > 0))
+    Console.WriteLine("Первая четверть ");
+ else if ((x < 0) && (y > 0))
+    Console.WriteLine("Вторая четверть ");
+ else if((x < 0) && (y < 0))
+    Console.WriteLine("Третья четверть ");
+ else if ((x > 0) && (y < 0))
+    Console.WriteLine("Четвертая четверть ");
+else
+    Console.WriteLine("Точка лежит на оси ");
+ 
