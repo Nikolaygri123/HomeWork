447027beda70f566ab93d7a3261f5e16e264fa9b
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-14 13:02:10 +0300

    Code

---
+++ Zadanie24/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+int a = 1;
+Console.WriteLine("Введите конечное число ");
+double b = Convert.ToDouble(Console.ReadLine());
+              while (a <= b)
+             {
+                 Console.WriteLine("Куб числа " + a);
+                 Console.WriteLine(("Число " + a * a * a));
+                 a++;
+             }
