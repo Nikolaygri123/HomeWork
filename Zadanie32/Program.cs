547b7244dadb88f13763ce1195e9a0a048fb5838
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-17 02:05:55 +0300

    Code

---
+++ Zadanie32/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задайте массив из восьми элементов, заполненных нулями и единицами. Выведите их на экран
+int[] massiv = new int[8];
+int lenght = massiv.Length;
+for( int index=0; index<lenght; index++)
+{
+    massiv[index] = new Random().Next(0,2);
+    Console.Write(massiv[index] + " " );
+}
