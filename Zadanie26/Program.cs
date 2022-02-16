ce926fbdbb95f1916c24e00a2f9a536f06163db1
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-17 00:45:21 +0300

    Code

---
+++ Zadanie26/Program.cs
@@ -0,0 +1,11 @@
+﻿// Возведите число А в натуральную степень B, используя цикл.
+Console.WriteLine("введите число ");
+double b =Convert.ToDouble(Console.ReadLine());
+
+for (int i = 2; i <= b; i++)
+{
+    for (int j = 2; j <=b; j++)
+    {
+    Console.WriteLine(Math.Pow(i,j));
+    }
+}
