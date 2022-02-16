ab845e621a52801a765ff110ea15d2fd17f82109
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-17 00:46:41 +0300

    Code

---
+++ Zadanie28/Program.cs
@@ -0,0 +1,12 @@
+﻿// Подсчитайте сумму цифр в числе.
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            while (a > 0)
+            {
+ 
+                s = s + a % 10;
+                a = a /10 ;
+ 
+            }
+ Console.WriteLine(s);
