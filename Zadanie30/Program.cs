8d7d7d3269ff9a1c8b8cb2edc2c853b99e4f4854
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-17 01:06:02 +0300

    Code

---
+++ Zadanie30/Program.cs
@@ -0,0 +1,11 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+int a = 0;
+Console.WriteLine("Введите конечное число ");
+int b = Convert.ToInt32(Console.ReadLine());
+              while (a <= b)
+             {
+                 if (a%2 ==0)
+                    Console.WriteLine("Куб числа " + a);
+                    Console.WriteLine(("Число " + a * a * a));
+                    a+=2;   
+             }
