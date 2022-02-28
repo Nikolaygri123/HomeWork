4342ba88bfc6321fa300361132a86c0690ccb1a0
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-02-28 12:15:43 +0300

    code

---
+++ Zadanie46/Program.cs
@@ -0,0 +1,23 @@
+﻿// Написать программу масштабирования фигуры
+string mass = "";
+
+for (int i = 1; i < 5; i++)
+{
+    string massiv = "";
+    Console.WriteLine($"введите {i} координаты ");
+    massiv = Console.ReadLine();
+    mass = $"{mass}({massiv})";
+}
+Console.Write(mass);
+Console.WriteLine();
+
+
+
+string[] array = mass.Split(' ', '-', ')', '(');
+int[] numbers = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+
+    Console.Write(array[i] + " ");
+}
+
