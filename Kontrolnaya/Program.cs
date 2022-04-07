a3bcb9c2046e9a78b71ec31a44778d70d0e05c2c
Николай Грищенко <koo123@MacBook-Air-Nikolaj.local>
2022-04-07 13:39:09 +0300

    code

---
+++ Kontrolnaya/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+string[] array =
+{
+    "строки",
+    "всякие",
+    "и",
+    "=)",
+    "и",
+    "=("
+};
+
+for (int i = 0; i < array.Length; i++)
+{
+
+    if (array[i].Length <= 3)
+    {
+        Console.WriteLine(array[i]);
+
+    }
+}
